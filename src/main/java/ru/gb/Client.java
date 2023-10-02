f115fed6ada471532d4205b03c34f8e802f15160
Silva0308 <v.travinina@gmail.com>
2023-10-02 18:56:06 +0300

    HW1 done

--- src/main/java/ru/gb/Client.java
+++ src/main/java/ru/gb/Client.java
@@ -1,2 +1,108 @@
-package ru.gb;public class Client {
+package ru.gb;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Client extends JFrame{
+
+    // константы, определяющие размеры и позицию окна
+    private static final int WINDOW_HEIGHT = 500;
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WIN_POS_X = 800;
+    private static final int WIN_POS_Y = 200;
+
+//элементы интерфейса: кнопки "Login" и "Send",
+// текстовые поля для ввода сообщений, адреса сервера, логина и пароля,
+// а также метка и панели для расположения элементов
+    JButton btnLog = new JButton("Login");
+    JButton btnSend = new JButton("Send");
+
+    JTextField msg = new JTextField();
+
+    JTextField txtId = new JTextField("some ID");
+    JTextField txtMask = new JTextField("some Mask");
+    JLabel labTemp = new JLabel();
+    JTextField txtLogin = new JTextField("Valery");
+    JPasswordField txtPass = new JPasswordField("74522");
+
+//конструктор
+    public Client(Server chatServ) {
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setLocation(WIN_POS_X, WIN_POS_Y);
+        setTitle("ChatClient");
+        setResizable(false);
+        JPanel panelReg1 = new JPanel(new GridLayout(2, 3));
+        panelReg1.add(txtId);
+        panelReg1.add(txtMask);
+        panelReg1.add(labTemp);
+        panelReg1.add(txtLogin);
+        panelReg1.add(txtPass);
+        panelReg1.add(btnLog);
+        add(panelReg1, BorderLayout.NORTH);
+        JPanel panelReg2 = new JPanel();
+        panelReg2.setLayout(new BoxLayout(panelReg2, BoxLayout.X_AXIS));
+        panelReg2.add(msg);
+        panelReg2.add(btnSend);
+        add(panelReg2, BorderLayout.SOUTH);
+
+        msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(chatServ.isStopped){
+                    try {
+                        addMessage(chatServ.panelMessage);
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else System.out.println("Сервер не отвечает");
+            }
+        });
+
+        //если флаг stopConst объекта chatServ равен true,
+        // вызывается метод addMessage с передачей ему объекта panelMessage.
+        // Если флаг равен false, выводится сообщение "Сервер не отвечает"
+        btnSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(chatServ.isStopped){
+                    try {
+                        addMessage(chatServ.panelMessage);
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else System.out.println("Сервер не отвечает");
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public void addMessage (JPanel jPanel) throws Exception{
+        String str = msg.getText();
+        if(str.length() == 0) throw new Exception("Пустая строка");
+        else{
+            JLabel chatMessage = new JLabel();
+            jPanel.add(chatMessage);
+            chatMessage.setText(txtLogin.getText() + ": " + str);
+            addTextFile(txtLogin.getText() + ": " + str);
+            jPanel.add(new JLabel(""));
+            msg.setText("");
+        }
+    }
+
+    public static void addTextFile(String str){
+        try(FileWriter writer = new FileWriter("chatServ.txt", true))
+        {
+            writer.write(str);
+            writer.append('\n');
+            writer.flush();
+        }
+        catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/main/java/ru/gb/Server.java
+++ src/main/java/ru/gb/Server.java
@@ -1,2 +1,98 @@
-package ru.gb;public class Server {
+package ru.gb;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+public class Server extends JFrame{
+
+    // константы, определяющие размеры и позицию окна
+    private static final int WINDOW_HEIGHT = 500;
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WIN_POS_X = 200;
+    private static final int WIN_POS_Y = 200;
+    boolean isStarted = true;
+    boolean isStopped = true;
+    JPanel panelMessage = new JPanel();
+
+    File file = new File("chatServ.txt"); // файл логов
+
+    JButton btnStart = new JButton("Start");
+    JButton btnStop = new JButton("Stop");
+
+    public Server() {
+        super("Server");
+        createGUI();
+    }
+
+    public void createGUI() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setLocation(WIN_POS_X, WIN_POS_Y);
+        setResizable(false);
+        JPanel panelBtn = new JPanel(new GridLayout(1, 2));
+        panelBtn.add(btnStart);
+        panelBtn.add(btnStop);
+        add(panelBtn, BorderLayout.SOUTH);
+        panelMessage.setLayout(new BoxLayout(panelMessage, BoxLayout.Y_AXIS));
+        add(panelMessage, BorderLayout.NORTH);
+        ActionListener actionListener1 = new btnStart();
+        btnStart.addActionListener(actionListener1);
+        ActionListener actionListener2 = new btnStop();
+        btnStop.addActionListener(actionListener2);
+        isStopped = false;
+        setVisible(true);
+    }
+
+    //запуск
+    public class btnStop implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if(isStopped){
+                panelMessage.removeAll();
+                panelMessage.repaint();
+                panelMessage.revalidate();
+                isStarted = true;
+                isStopped = false;
+            }
+            else {
+                System.out.println("Сервер уже остановлен");
+            }
+        }
+    }
+
+    //остановка
+    public class btnStart implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (isStarted) {
+                if(file.exists()){
+                    try {
+                        chatArchive(file, panelMessage);
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                isStarted = false;
+                isStopped = true;
+            }
+            else {
+                System.out.println("Сервер уже запущен");
+            }
+        }
+    }
+//метод, который отвечает за загрузку архива чата из файла и отображение сообщений на панели.
+    public static void chatArchive (File file, JPanel jPanel) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null) {
+            JLabel gg = new JLabel();
+            jPanel.add(gg);
+            gg.setText(st);
+        }
+    }
 }
+
